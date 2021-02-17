280dbf18b674ab6d2eb5f66c87b5730f247dcc4f
Cliff Brake <cbrake@bec-systems.com>
2021-02-17 15:13:18 -0500

    also fix up markdown links

--- envsetup.sh
+++ envsetup.sh
@@ -17,7 +17,15 @@ siot_update_docs() {
     dest=content/$project
     rm -rf $dest
     mkdir "$dest"
-    cp "$src"/*.md "$dest/"
+    for f in "$src"/*.md; do
+      file_name=$(basename "$f")
+      echo "file_name: $file_name"
+      if [ "$file_name" = "README.md" ]; then
+        cp "$f" "$dest/_index.md"
+      else
+        cp "$f" "$dest/"
+      fi
+    done
     cp "$src"/*.png "$dest/"
     cp "$src"/*.jpg "$dest/"
 
@@ -26,8 +34,13 @@ siot_update_docs() {
     for f in "$dest"/*.md; do
       echo "processing $f"
       siot_update_image_links "$f"
+
+      # prefix images with parent path
       sed -i "s/(\([A-Za-z0-9_-]*\.png\))/(\/${project}\/\1)/g" "$f"
       sed -i "s/(\([A-Za-z0-9_-]*\.jpg\))/(\/${project}\/\1)/g" "$f"
+
+      # remove md extension from links to markdown files
+      sed -i "s/(\([A-Za-z0-9_-]*\)\.md)/(\1)/g" "$f"
     done
   done
 }
